700df76d4ab352bcd5be8305e09d3576ac1ce6e4
bitterButler <b.hudson.mark@gmail.com>
2023-10-06 11:17:29 +0200

    second commit - while loop, and if statement used, but still not finished

--- GreatestCommonDivisor.py
+++ GreatestCommonDivisor.py
@@ -5,28 +5,8 @@ def GreatComDivisor(a, b):
     #         225->(360-135) = Rest * 1 + 90 (Rest = 90)
     #         135->(225-90) =  Rest * 1 + 45 ...
 
-    # Rest = a % b  # 360 % 225
-    # print("Rest: ", Rest)  # = 135
-    #
-    # a = a - Rest  # 360-135 = 225
-    # print("a :", a)
-    # Rest = a % Rest  # 225 % 135
-    # print("Rest: ", Rest)  # Rest= 90
-    #
-    # a = a - Rest  # 225-90 = 135
-    # print("a :", a)
-    # Rest = a % Rest  # 135 % 90
-    # print("Rest: ", Rest)  # Rest= 45
-    #
-    # a = a - Rest  # 135-45 = 90
-    # print("a :", a)
-    # Rest = a % Rest  # 90 % 45 = 0, cuz 2x 45 = 90 and Rest = 0
-    # print("Rest:", Rest)
-
     # with whiles, and if-else
-
     print("a / b: ", a % b)
-
     while (b > 0):  # 135 > 0
         R = a % b  # R = 360 % 225 -> 135
         if b > R:  # 225 > 135
